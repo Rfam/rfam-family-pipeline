6701c1fec84eae9a67f004aa66a147c127d08fdf
ikalvari <ikalvari@ebi.ac.uk>
2017-10-06 15:22:50 +0100

    Add module to populate FamilyAuthor table

---
+++ Rfam/Schemata/RfamLive/ResultSet/FamilyAuthor.pm
@@ -0,0 +1,43 @@
+package RfamLive::ResultSet::FamilyAuthor;
+
+use strict;
+use warnings;
+use Carp;
+
+use base 'DBIx::Class::ResultSet';
+
+sub find_or_create_authors {
+	my ($self, $familyObj) = @_;
+  
+  	if(!$familyObj or !$familyObj->isa('Bio::Rfam::Family')){
+    croak('Either the Bio::Rfam::Family object was undefined or not an object of that type.');
+  }
+
+  # delete family records if any
+  my $family_records = $self->search({rfam_acc => $familyObj->{DESC}->{AC}});
+  	if(defined($family_records)){
+	$family_records->delete();
+  }
+
+  if(defined($familyObj->DESC->AU)){
+    foreach my $author (@{$familyObj->DESC->AU}){
+		# my $author_name = defined($author->{name}) ? $db->{comment} : '';
+		my $orcid = defined($author->{orcid}) ? $author->{orcid} : '';
+	
+	my $new_author = $self->find_or_create( { rfam_acc => $familyObj->{DESC}->{AC},
+                                          	  name  => $author->{name},
+                                              orcid => $author->{orcid},
+                                              order => $author->{order}},
+					  						  {key => 'author_id'});
+      	
+	if(!defined($new_author)){	
+		croak("Error updating family authors for $familyObj->{DESC}->{AC}");
+    }
+	else{
+  		$new_author->update({orcid => $orcid}, {key => 'composite_key'});
+	}
+	}
+}
+}
+
+1;	
