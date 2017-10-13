915d8bbff1b645316afa9e1c1df1733cafde771d
ikalvari <ikalvari@ebi.ac.uk>
2017-10-13 17:56:43 +0100

    Corrected function to match table structure

--- Rfam/Schemata/RfamLive/ResultSet/FamilyAuthor.pm
+++ Rfam/Schemata/RfamLive/ResultSet/FamilyAuthor.pm
@@ -5,6 +5,7 @@ use warnings;
 use Carp;
 
 use base 'DBIx::Class::ResultSet';
+use RfamLive::ResultSet::Author;
 
 sub find_or_create_authorsFromFamilyObj {
 	my ($self, $familyObj) = @_;
@@ -12,6 +13,9 @@ sub find_or_create_authorsFromFamilyObj {
   	if(!$familyObj or !$familyObj->isa('Bio::Rfam::Family')){
     croak('Either the Bio::Rfam::Family object was undefined or not an object of that type.');
   }
+
+  # create an instance of the author table
+  my $author_tbl=RfamLive::ResultSet::Author->new();
 
   # delete family records if any
   my $family_records = $self->search({rfam_acc => $familyObj->{DESC}->{AC}});
@@ -24,9 +28,27 @@ sub find_or_create_authorsFromFamilyObj {
 		# my $author_name = defined($author->{name}) ? $db->{comment} : '';
 		my $orcid = defined($author->{orcid}) ? $author->{orcid} : '';
 	
-	my $new_author = $self->find_or_create( { rfam_acc => $familyObj->{DESC}->{AC},
-                                          	  name  => $author->{name},
-                                              orcid => $author->{orcid},
+    # now search for author_id
+    my $author_id;
+    # first search by name. 
+    # This one should do the trick as author creation precedes this update
+    my $author_entry = $author_tbl->find({name => $author->{name}});
+    if (defined($author_entry)){
+      $author_id=$author_entry->{'author_id'};
+    }
+    # search in synonyms
+    else{
+      my $author_entry = $author_tbl->search_like({synonyms => '%$author->{name}%'});
+      if (defined($author_entry)){
+        $author_id=$author_entry->{'author_id'};
+      }
+    }
+    
+
+
+	  # search for author id 
+    my $new_author = $self->find_or_create( { rfam_acc => $familyObj->{DESC}->{AC},
+                                          	  author_id  => $author_id,
                                               order => $author->{order}});
       	
 	if(!defined($new_author)){	
