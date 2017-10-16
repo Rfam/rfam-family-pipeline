f8d7824969e23fb83c6170d1dffea822ee2cdb87
ikalvari <ikalvari@ebi.ac.uk>
2017-10-16 17:15:10 +0100

    Added Author subroutine to return author ids

--- Rfam/Schemata/RfamLive/ResultSet/Author.pm
+++ Rfam/Schemata/RfamLive/ResultSet/Author.pm
@@ -51,4 +51,35 @@ sub create_or_updateAuthorFromFamilyObj {
     }
   }
 }
+
+sub get_author_ids{
+
+  my ($self, $familyObj) = @_;
+
+  if(!$familyObj or !$familyObj->isa('Bio::Rfam::Family')){
+    croak('Either the Bio::Rfam::Family object was undefined or not an object of that type.');
+  }
+
+  my %author_ids;
+
+  if(defined($familyObj->DESC->AU)){
+    foreach my $author (@{$familyObj->DESC->AU}){ 
+      # search author table by author name
+      my $author_entry = $self->find({name => $author->{name}});
+      # if not found
+      if(!defined $author_entry){
+        # search for an author by synonym
+        my $author_entry = $self->search_like({synonyms => '%$author->{name}%'});
+        if(!defined $author_entry){
+          croak('Aurhor $author->{name} does not exist in the database.');
+        }
+      }else{
+        # update hash
+      $author_ids{$author->{name}}=$author_entry->{'author_id'};
+      }
+    }
+  }
+  return %author_ids;
+} # sub
+
 1;
