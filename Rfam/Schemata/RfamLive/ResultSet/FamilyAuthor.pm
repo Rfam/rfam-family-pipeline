7951709c5afaf31d2516f8d39033d4258bc4e3c9
ikalvari <ikalvari@ebi.ac.uk>
2017-10-19 15:26:57 +0100

    Finalized function to populate the table

--- Rfam/Schemata/RfamLive/ResultSet/FamilyAuthor.pm
+++ Rfam/Schemata/RfamLive/ResultSet/FamilyAuthor.pm
@@ -14,6 +14,7 @@ sub find_or_create_authorsFromFamilyObj {
   }
 
   # create an instance of the author table
+  my $schema = $self->result_source->schema;
   my $author_tbl= $schema->resultset('Author');
 
   # delete family records if any
@@ -23,41 +24,23 @@ sub find_or_create_authorsFromFamilyObj {
   }
 
   if(defined($familyObj->DESC->AU)){
+  
+    my $author_ids = $author_tbl->get_author_ids($familyObj);
+
     foreach my $author (@{$familyObj->DESC->AU}){
-		# my $author_name = defined($author->{name}) ? $db->{comment} : '';
-		my $orcid = defined($author->{orcid}) ? $author->{orcid} : '';
-	
-    # now search for author_id
-    my $author_id;
-    # first search by name. 
-    # This one should do the trick as author creation precedes this update
-    my $author_entry = $author_tbl->find({name => $author->{name}});
-    if (defined($author_entry)){
-      $author_id=$author_entry->{'author_id'};
-    }
-    # search in synonyms
-    else{
-      my $author_entry = $author_tbl->search_like({synonyms => '%$author->{name}%'});
-      if (defined($author_entry)){
-        $author_id=$author_entry->{'author_id'};
-      }
-    }
-    
+		
+      # populate family author_table
+      my $new_author = $self->find_or_create( { rfam_acc => $familyObj->{DESC}->{AC},
+                                              author_id  => $author_ids->{$author->{name}},
+                                              desc_order => $author->{order}});
 
+       if(!defined($new_author)){
+                croak("Error updating family authors for $familyObj->{DESC}->{AC}");
+        }
 
-	  # search for author id 
-    my $new_author = $self->find_or_create( { rfam_acc => $familyObj->{DESC}->{AC},
-                                          	  author_id  => $author_id,
-                                              order => $author->{order}});
-      	
-	if(!defined($new_author)){	
-		croak("Error updating family authors for $familyObj->{DESC}->{AC}");
-    }
-	else{
-  		$new_author->update({orcid => $orcid});
-	}
-	}
+	  } 
+  }
 }
-}
+
 
 1;	
