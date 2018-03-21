51a422d998404357ceb23e3309a54ea1c361931e
ikalvari <ikalvari@ebi.ac.uk>
2018-03-21 15:15:04 +0000

    New code to generate bed files directly from genome tblout

---
+++ Rfam/Schemata/RfamLive/ResultSet/Genseq.pm
@@ -0,0 +1,48 @@
+
+package RfamLive::ResultSet::Genseq;
+
+use strict;
+use warnings;
+use Carp;
+use utf8;
+
+use base 'DBIx::Class::ResultSet';
+
+# TO DO: need to simplify the code and add more checks
+
+sub get_chromosome_label_for_genome_browser_hub {
+  my ($self, $upid, $rfamseq_acc) = @_;
+
+  my $chromosome_label='';
+
+  # check if the rfamseq_acc exists in Genseq table
+  my $rfamseq_acc_entry = $self->find({rfamseq_acc => $rfamseq_acc,
+                                          upid => $upid});
+  # if no entry found, return empty string
+  if(!defined($rfamseq_acc_entry)){
+      #croak("No entry found in Genseq for rfamseq_acc: $rfamseq_acc");
+      return $chromosome_label;
+  }
+
+  else{
+
+    # fetch column values
+    my $chromosome_name = $rfamseq_acc_entry->get_column('chromosome_name');
+    my $chromosome_type = $rfamseq_acc_entry->get_column('chromosome_type');
+
+    # 1. check if chromosome name and chromosome type are null and return an empty string
+    if ($chromosome_name eq '' or $chromosome_type eq ''){
+      #croak("No chromosome label could be created for rfamseq_acc $rfamseq_acc");
+      return $chromosome_label;
+    }
+    
+    # construct chromosome label
+    else{
+      $chromosome_label = "chr" . $chromosome_name;
+    }
+  }
+
+  return $chromosome_label;
+ }
+
+1;
---
+++ Rfam/Scripts/curation/tblout2bigBedGenomes.pl
@@ -0,0 +1,84 @@
+#!/usr/bin/env perl
+
+#parses tblout files produced by cmsearch into bigBed files for genome browser hub
+#use: tblout2bigBed.pl infile db
+#infile is name of input tblout file, db is assembly name (from UCSC)
+
+use strict;
+use warnings;
+use Data::Dumper;
+use DDP;
+use Bio::Rfam::Config;
+
+
+my $infile = $ARGV[0];
+#my $db = $ARGV[1];
+
+
+my $config = Bio::Rfam::Config->new;
+my $rfamdb = $config->rfamlive;
+
+my $upid = substr $infile, 0, 11;
+
+my $bedfile = $upid . ".bed";
+
+#create BED format file
+#open bed file and write header
+open (BED, ">$bedfile") or die "Cannot open file $bedfile $!\n";
+
+#open infile and parse
+
+open (IN, "$infile") or die "Cannot open file $infile $!\n";
+
+while (<IN>){
+
+#skip lines beginning #
+	unless ($_ =~ /^#/){
+		my @data = (split/\s+/,$_);
+#need to ignore those where chrom is non-standard
+		
+		# working with the rfamseq_acc here. Split target and use third item in the table. 
+		my $accession = $data[0];
+		my @target = (split/\|/,$accession);
+		my $rfamseq_acc = $target[2];
+
+		my $chromosome_label;
+		$chromosome_label = $rfamdb->resultset('Genseq')->get_chromosome_label_for_genome_browser_hub($upid, $rfamseq_acc);
+
+
+		# jump to next hit if chromosome label is empty or does not comply with the expected format
+		if ($chromosome_label eq '' or $chromosome_label =~ /chr\w{1,2}_\S+/){
+			next;
+		}
+
+		#start must be lower than end - so the two need reversing if strand = '-'
+		if ($data[9] eq '+'){
+	   	  	print BED "$chromosome_label\t$data[7]\t$data[8]\t$data[2]\n";
+			#print BED "$data[3]\t$data[0]\t$data[7]\t$data[8]\t$data[2]\n";
+		} elsif ($data[9] eq '-'){
+	   		print BED "$chromosome_label\t$data[8]\t$data[7]\t$data[2]\n";
+			#print BED "$data[3]\t$data[0]\t$data[8]\t$data[7]\t$data[2]\n";
+		} else {
+	    		print "Strand character unrecognised in line: $_";
+		}
+
+	}
+
+} #end of loop through infile
+ 
+close (IN);
+close (BED);
+
+#sort BED file chrom then chromStart: sort -k1,1 -k2,2n unsorted.bed > input.bed
+
+my $sortedfile = $upid . "_sorted.bed";
+system("sort -k1,1 -k2,2n $bedfile > $sortedfile");
+
+# DO this later - need a script to replace UCSC's fetchChromSizes
+#use fetchChromSizes to create chrom.sizes file
+#system("/nfs/production/xfam/rfam/software/fetchChromSizes $db > chrom.sizes") and die "Could not create chrom.sizes for $db $!\n";
+
+
+#use bedToBigBed to convert BED to bigBed
+#my $bigbedfile = $infile . ".bb";
+#system ("/nfs/production/xfam/rfam/software/bedToBigBed $sortedfile chrom.sizes $bigbedfile") and die "Could not convert BED to bigBed $!\n";
