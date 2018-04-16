244982a21e4e15d759c91ab287526bea5800acea
ikalvari <ikalvari@ebi.ac.uk>
2018-04-16 17:02:32 +0100

    New function to fetch sequence length

--- Rfam/Schemata/RfamLive/ResultSet/Rfamseq.pm
+++ Rfam/Schemata/RfamLive/ResultSet/Rfamseq.pm
@@ -2,6 +2,8 @@ package RfamLive::ResultSet::Rfamseq;
 
 use strict;
 use warnings;
+use Carp;
+use utf8;
 
 use base 'DBIx::Class::ResultSet';
 
@@ -16,5 +18,15 @@ sub seqaccToTaxon {
   return (defined($row) ? $row : undef);
 }
 
+
+# to be used 
+sub get_sequence_length {
+	my ($self, $rfamseq_acc) = @_;
+
+	my $rfamseq_row = $self->find({rfamseq_acc => $rfamseq_acc});
+	my $seq_length = $rfamseq_row->get_column('length');
+
+	return $seq_length;
+}
 
 1;
