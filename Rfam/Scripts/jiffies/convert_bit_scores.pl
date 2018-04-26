4a9c7f7c9cb0f49a318653184916d6e8a67ad19f
ikalvari <ikalvari@ebi.ac.uk>
2018-04-26 17:07:34 +0100

    Add cases get e-values in infernal like format

--- Rfam/Scripts/jiffies/convert_bit_scores.pl
+++ Rfam/Scripts/jiffies/convert_bit_scores.pl
@@ -26,13 +26,28 @@ my $new_evalue = 0.0;
 my $bit_score = 0;
 
 my $opt = "-Z --rfam --nohmmonly --notextw --cut_ga --tblout --mpi --acc";
-
+my $new_evalue_sn;
 # may need to change $config->seqdbConfig("rfamseq")->{"dbSize"} to a command line argument
 foreach my $region (@$regions) {
 	$bit_score = $region->[4];
 	$new_evalue = Bio::Rfam::Infernal::cm_bitsc2evalue($familyIO_obj->{'CM'}, $bit_score, $config->seqdbConfig("rfamseq")->{"dbSize"}, $opt); 
 	# convert the new e-value to scientific notation
-	my $new_evalue_sn = sprintf("%.1e", $new_evalue);
+	if ($new_evalue < 1){
+		# count floating points
+		my $temp_string = sprintf("%.5f", $new_evalue);
+		my $count = () = $temp_string =~ /0/g;
+		# if 0s are more than 4 convert to scientific notation
+		if ($count > 4){
+			$new_evalue_sn = sprintf("%.1e", $new_evalue);
+		}
+		# keep float otherwise
+		else{
+			$new_evalue_sn=$temp_string;
+		}
+	# if number is > 0, keep 1 floating point
+	}else{
+		$new_evalue_sn = sprintf("%.1f", $new_evalue);
+	}
 	print "$rfam_acc\t$region->[3]\t$region->[1]\t$region->[2]\t$region->[4]\t$new_evalue_sn\t$region->[6]\t$region->[7]\t$region->[8]\t$region->[9]\t$region->[10]\n";
 	$bit_score = 0;
 	$new_evalue = 0.0;
