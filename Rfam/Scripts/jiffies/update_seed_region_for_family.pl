5cbc830092129a8593267d7377e77ecf3bb10df6
Eric Nawrocki <nawrocke@ncbi.nlm.nih.gov>
2019-02-27 21:58:19 +0000

    Adds script that might work to update md5s in seedregion table?

---
+++ Rfam/Scripts/jiffies/update_seed_region_for_family.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Bio::Rfam::Config;
+use Bio::Rfam::FamilyIO;
+use Bio::Rfam::SVN::Client;
+use Data::Printer;
+
+my $family = shift;
+
+unless(defined($family)){
+  die "Please provide a family\n";
+}
+print "$family\n";
+
+my $config = Bio::Rfam::Config->new;
+my $familyIO = Bio::Rfam::FamilyIO->new;
+my $rfamdb = $config->rfamlive;
+my $client = Bio::Rfam::SVN::Client->new({config => $config});
+
+#Need to put a transaction around this block
+my $guard = $rfamdb->txn_scope_guard;
+
+my $familyObj = $familyIO->loadRfamFromSVN($family, $client);
+print STDERR "Successfully loaded SVN copy of $family through middleware\n";
+
+# call subroutine that we modified to additionally calculate md5s
+$rfamdb->resultset('SeedRegion')->updateSeedRegionsFromFamilyObj( $familyObj );
+
+$guard->commit;
+
+exit 0;
