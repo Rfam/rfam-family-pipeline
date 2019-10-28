f4978ee1e6331e4ee7739a973f5e3d9b23518b82
ikalvari <ikalvari@ebi.ac.uk>
2019-10-28 16:40:13 +0000

    Abstracts for rfam user account creation

---
+++ kubernetes/scripts/add_user.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+# ------------------------------------------------------------------
+
+
+def create_new_rfam_user(expire_date, group):
+	# Create a user with password and shell information
+	# potentially load this information from the database
+	pass
+
+# ------------------------------------------------------------------
+
+def setup_home_dir():
+	# creates .kube dir and sets the config file
+	pass
+# ------------------------------------------------------------------
+
+
+def create_user_pvc(username, size=2):
+	pass
+
+# ------------------------------------------------------------------
+
+def create_new_user_login_deployment():
+	pass
+
+# ------------------------------------------------------------------
+
+
+if __name__=='__main__':
+
+	pass
