2a814fb9e68b93a635b07ddeaad393fa81dae064
ikalvari <ikalvari@ebi.ac.uk>
2019-10-29 14:58:17 +0000

    Replaced k8s deployment string with the one in k8s manifest lib

--- kubernetes/python_code/scripts/rfcloud.py
+++ kubernetes/python_code/scripts/rfcloud.py
@@ -7,11 +7,11 @@ import subprocess
 import getpass
 import socket
 
+import lib.k8s_manifests as k8s_lib
 from subprocess import Popen, PIPE
 #from kubernetes import client, config, utils
 
 # --------------------------------------------------------------------------------------------
-
 
 def create_new_user_login_pod(username):
 	"""
@@ -27,48 +27,7 @@ def create_new_user_login_pod(username):
 	config.load_incluster_config()
     	k8s_client = client.ApiClient()
 
-	k8s_deployment_str = """
-apiVersion: apps/v1
-kind: Deployment
-metadata:
-  name: rfam-login-pod-USERID
-  labels:
-    app: rfam-family-builder-USERID
-    tier: frontend
-spec:
-  replicas: 1
-  selector:
-    matchLabels:
-      app: rfam-family-builder-USERID
-  template:
-    metadata:
-      name: rfam-login-pod-USERID
-      labels:
-        app: rfam-family-builder-USERID
-        user: USERID
-        tier: frontend  
-    spec:
-      containers:
-      - name: rfam-login-pod-USERID
-        image: ikalvari/rfam-cloud:kubes
-        imagePullPolicy: Always
-        ports:
-        - containerPort: 9876
-        volumeMounts:
-        - name: rfam-login-pod-storage-USERID # this one must match the volume name of the pvc
-          mountPath: /workdir
-        - name: nfs-pv
-          mountPath: /Rfam/rfamseq
-        stdin: true
-        tty: true
-      volumes:
-      - name: rfam-login-pod-storage-USERID
-        persistentVolumeClaim:
-          claimName: rfam-pvc-USERID # this one must match the pvc name
-      - name: nfs-pv
-        persistentVolumeClaim:
-          claimName: nfs-pvc
-      restartPolicy: Always"""
+	k8s_deployment_str = k8s_lib.user_login_deployment_str
 
 	k8s_deployment_str = k8s_deployment_str.replace("USERID", username)
 
@@ -80,6 +39,7 @@ spec:
 
 	#k8s_api = utils.create_from_yaml(k8s_client, login_deployment)
 	subprocess.call("kubectl create -f %s" % login_deployment, shell=True)
+
 # --------------------------------------------------------------------------------------------
 
 def check_k8s_login_deployment_exists(username):
